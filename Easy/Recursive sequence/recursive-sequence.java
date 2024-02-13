24f78dd5bd3aac2ba9b1adb0911fd8b8a83ba4fe
Tejas Ganesh Kotkar <128985845+kTejss@users.noreply.github.com>
2024-02-13 20:12:38 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Recursive sequence/recursive-sequence.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            
+            Solution ob = new Solution();
+            System.out.println(ob.sequence(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static long sequence(int n){
+        long MOD = 1000000007;
+        long x = 1, result = 0;
+
+        for (int i = 1; i <= n; ++i) 
+        {
+            long temp = 1;
+
+            for (int j = 1; j <= i; ++j) 
+            {
+                temp = (temp * x) % MOD;
+                x++;
+            }
+
+            result = (result + temp) % MOD;
+        }
+
+        return result;
+    }
+}
