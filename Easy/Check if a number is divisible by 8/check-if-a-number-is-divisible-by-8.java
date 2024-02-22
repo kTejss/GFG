0a91cb6f63d7493f6b8bb9277e0c97bb7ceae73c
Tejas Ganesh Kotkar <128985845+kTejss@users.noreply.github.com>
2024-02-22 17:40:16 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Check if a number is divisible by 8/check-if-a-number-is-divisible-by-8.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S=read.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.DivisibleByEight(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    int DivisibleByEight(String S){
+        int n = S.length();
+        int result = 0;
+        for(int i=0;i<n;i++)
+        {
+            result = ((result*10)+S.charAt(i)-'0')%8;
+        }
+        if(result == 0)
+            return 1;
+        return -1;
+    }
+}
