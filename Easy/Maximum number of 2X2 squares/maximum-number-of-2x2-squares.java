60ee92c4978b565a1f8585658c4c68be9afecf60
Tejas Ganesh Kotkar <128985845+kTejss@users.noreply.github.com>
2024-02-29 15:49:37 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximum number of 2X2 squares/maximum-number-of-2x2-squares.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());//testcases
+        
+        while(t-- > 0)
+        {
+            long n = Long.parseLong(read.readLine());//input n
+            
+            Solution ob= new Solution();
+            System.out.println(ob.numberOfSquares(n));
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public static long numberOfSquares(long base)
+    {
+        if(base==2)
+
+        return 0;
+
+        base=base-2;
+
+        base=base/2;
+
+        return base*(base+1)/2;
+    }
+}
