c05269e7571d2c5cf3a5052a1fd88addbffcc1fa
Tejas Ganesh Kotkar <128985845+kTejss@users.noreply.github.com>
2024-03-01 20:58:23 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Is Square/is-square.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            
+            int x1 = Integer.parseInt(S[0]);
+            int y1 = Integer.parseInt(S[1]);
+            int x2 = Integer.parseInt(S[2]);
+            int y2 = Integer.parseInt(S[3]);
+            int x3 = Integer.parseInt(S[4]);
+            int y3 = Integer.parseInt(S[5]);
+            int x4 = Integer.parseInt(S[6]);
+            int y4 = Integer.parseInt(S[7]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.isSquare(x1,y1,x2,y2,x3,y3,x4,y4));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static String isSquare(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+         if(x1 == x2 && x3 == x4 && y1 == y2 && y3 == y4)
+           return "No";
+        int top = Math.abs(x1 - x2);
+        int bottom = Math.abs (x3 - x4);
+        int left = Math.abs(y1 - y3);
+        int right =Math.abs (y2 - y4);
+        if(left == right && top == bottom) {
+            return "Yes";
+        }
+        else {
+            return "No";
+        }
+    }
+};
