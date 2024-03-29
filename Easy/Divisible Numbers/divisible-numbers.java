c95db231ab7f9707c860b6004ec1f7987409d014
Tejas Ganesh Kotkar <128985845+kTejss@users.noreply.github.com>
2024-03-29 19:22:14 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Divisible Numbers/divisible-numbers.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String S[] = read.readLine().split(" ");
+            long A = Long.parseLong(S[0]);
+            long B = Long.parseLong(S[1]);
+            Solution ob = new Solution();
+            System.out.println(ob.find(A,B));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static long find(long A,long B){  
+        long x= A+1;
+        while(x%B!=0){
+            x=x+1;
+        }
+        return x;
+    } 
+}
