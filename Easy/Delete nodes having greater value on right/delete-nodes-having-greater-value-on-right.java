cfdc2f057bcd35b977332f0d3a808aa65ee2dd4c
Tejas Ganesh Kotkar <128985845+kTejss@users.noreply.github.com>
2024-05-19 18:18:08 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Delete nodes having greater value on right/delete-nodes-having-greater-value-on-right.java
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+public class LinkedList
+{
+    
+ 
+
+    /* Function to print linked list */
+   public static void print(Node root)
+    {
+        Node temp = root;
+        while(temp!=null)
+        {
+            System.out.print(temp.data + " ");
+            temp=temp.next;
+        }
+    }
+
+
+    /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+
+        /* Constructed Linked List is 1.2.3.4.5.6.
+         7.8.8.9.null */
+        int value;
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0 && sc.hasNextInt() )
+        {
+            int n = sc.nextInt();
+            
+            //int n=Integer.parseInt(br.readLine());
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+
+            for (int i = 1; i < n && sc.hasNextInt() ; i++)
+            {
+                int a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+
+            Node result = new Solution().compute(head);
+            print(result);
+            System.out.println();
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node {
+   int data;
+   Node next;
+
+  Node(int data) {
+      this.data = data;
+  }
+}
+*/
+class Solution
+{
+    Node compute(Node head)
+    {
+        Node curr = head;
+        
+        Node newnode = new Node(0);
+        Node again = newnode;
+        while(curr!=null){
+            
+            if(!checkIf(curr)){
+                again.next = curr;
+                again = again.next;
+                
+            }
+            
+            curr = curr.next;
+            
+        }
+        
+        return newnode.next;
+        
+    }
+    
+    boolean checkIf(Node cur){
+        Node prev  = cur;
+        while(cur!=null){
+            if(prev.data<cur.data){
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+}
+  
