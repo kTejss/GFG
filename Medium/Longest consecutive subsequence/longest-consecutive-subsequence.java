87803a211f1e71827b6e9359134db94e1a6f76d9
Tejas Ganesh Kotkar <128985845+kTejss@users.noreply.github.com>
2024-06-09 20:42:57 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest consecutive subsequence/longest-consecutive-subsequence.java
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			
+			for(int i=0; i<n; i++)
+				a[i] = sc.nextInt();
+		    out.println(new Solution().findLongestConseqSubseq(a, n));
+		    t--;
+		}
+		out.close();
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{   
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+	static int findLongestConseqSubseq(int arr[], int N)
+	{
+	    TreeSet<Integer> tSet = new TreeSet<>();
+        for (int i = 0; i < N; i++) {
+            tSet.add(arr[i]);
+        }
+ 
+        int prevElement = Integer.MIN_VALUE;
+        int maxLen = 1;
+        int count = 0;
+ 
+        for (Integer value : tSet) {
+            if (value - 1 == prevElement) {
+                count++;
+                prevElement = value;
+            }
+            else if (value != prevElement) {
+                count = 1;
+                prevElement = value;
+            }
+            maxLen = Math.max(maxLen, count);
+        }
+        return maxLen;
+	}
+}
