f03f00d696f93bedd0a42062554e80b4a12adadc
Tejas Ganesh Kotkar <128985845+kTejss@users.noreply.github.com>
2024-07-15 23:52:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest number/smallest-number.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+
+        while (tc-- > 0) {
+            int s = sc.nextInt();
+            int d = sc.nextInt();
+
+            Solution obj = new Solution();
+            String res = obj.smallestNumber(s, d);
+
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution {
+    public String smallestNumber(int s, int d) {
+        StringBuilder res = new StringBuilder();
+        int i = 1;
+        while(i<=d){
+            int num = s-(d-i)*9<=0?0:s-(d-i)*9;
+            if(num>9){
+                return "-1";
+            }
+            if(i==1 && num == 0){
+                num = 1;
+            }
+            res.append((char)(num+'0'));
+            s-=num;
+            i++;
+        }
+        return res.toString();
+    }
+}
