02b37f1ba6f61a162762274cb7e07f96334a5209
Tejas Ganesh Kotkar <128985845+kTejss@users.noreply.github.com>
2024-07-23 00:07:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.java
@@ -0,0 +1,192 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
+
+class GFG {
+    static Node buildTree(String str) {
+        // Corner Case
+        if (str.length() == 0 || str.equals('N')) return null;
+        String[] s = str.split(" ");
+
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+
+        // Starting from the second element
+        int i = 1;
+        while (!q.isEmpty() && i < s.length) {
+            // Get and remove the front of the queue
+            Node currNode = q.remove();
+
+            // Get the curr node's value from the string
+            String currVal = s[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the curr node
+                currNode.left = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= s.length) break;
+            currVal = s[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the curr node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.right);
+            }
+
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t > 0) {
+            String s = br.readLine();
+            Node root1 = buildTree(s);
+
+            s = br.readLine();
+            Node root2 = buildTree(s);
+
+            Solution T = new Solution();
+            List<Integer> ans = T.merge(root1, root2);
+            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
+            System.out.println();
+
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node left, right;
+
+    public Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}
+
+*/
+class Solution {
+    // Function to return a list of integers denoting the node
+    // values of both the BST in a sorted order.
+    public List<Integer> merge(Node root1, Node root2) {
+        List<Integer>a=new ArrayList<>();
+
+        if(root1==null){
+
+            return a;
+
+        }
+
+        else{
+
+            Queue<Node>q1=new LinkedList<>();
+
+            q1.add(root1);
+
+            while(!q1.isEmpty()){
+
+                Node node1=q1.poll();
+
+                a.add(node1.data);
+
+                if(node1.left!=null){
+
+                    q1.add(node1.left);
+
+                }
+
+                if(node1.right!=null){
+
+                    q1.add(node1.right);
+
+                }
+
+            }
+
+        }       
+
+        List<Integer>b=new ArrayList<>();
+
+        if(root2==null){
+
+            return b;
+
+        }
+
+        else{
+
+            Queue<Node>q2=new LinkedList<>();
+
+            q2.add(root2);
+
+            while(!q2.isEmpty()){
+
+                Node node2=q2.poll();
+
+                b.add(node2.data);
+
+                if(node2.left!=null){
+
+                    q2.add(node2.left);
+
+                }
+
+                if(node2.right!=null){
+
+                    q2.add(node2.right);
+
+                }
+
+            }
+
+        }
+
+        a.addAll(b);
+
+        Collections.sort(a);
+
+        return a;
+    }
+}
